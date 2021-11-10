2394569d9d0edbbad0573f17e534f57763062398
gaojingsong <jg2109@georgetown.edu>
2021-11-10 12:26:43 -0500

    feat: generate wiki embedding based on BERT

---
+++ model_code/retriever_bert_doc_embedding.py
@@ -0,0 +1,60 @@
+import math
+from time import time
+
+import datasets
+import numpy as np
+import torch
+
+from model_code.retriever_bert_qa_embedding import make_qa_retriever_model
+
+
+def embed_passages_for_retrieval(passages, qa_tokenizer, qa_embedding, max_length=128, device="cuda:0"):
+    a_toks = qa_tokenizer.batch_encode_plus(passages, max_length=max_length, truncation=True, padding='max_length')
+    a_ids, a_mask = (
+        torch.LongTensor(a_toks["input_ids"]).to(device),
+        torch.LongTensor(a_toks["attention_mask"]).to(device),
+    )
+    with torch.no_grad():
+        a_reps = qa_embedding.embed_answers(a_ids, a_mask).cpu().type(torch.float)
+    return a_reps.numpy()
+
+
+def embed_questions_for_retrieval(q_ls, qa_tokenizer, qa_embedding, device="cuda:0"):
+    q_toks = qa_tokenizer.batch_encode_plus(q_ls, max_length=128, truncation=True, padding='max_length')
+    q_ids, q_mask = (
+        torch.LongTensor(q_toks["input_ids"]).to(device),
+        torch.LongTensor(q_toks["attention_mask"]).to(device),
+    )
+    with torch.no_grad():
+        q_reps = qa_embedding.embed_questions(q_ids, q_mask).cpu().type(torch.float)
+    return q_reps.numpy()
+
+
+def make_qa_dense_index(
+    qa_embedding,
+    qa_tokenizer,
+    passages_set,
+    batch_size=512,
+    max_length=128,
+    index_name="kilt_passages_reps.dat",
+    dtype="float32",
+    device="cuda:0",
+):
+    st_time = time()
+    fp = np.memmap(index_name, dtype=dtype, mode="w+", shape=(passages_set.num_rows, 128))
+    n_batches = math.ceil(passages_set.num_rows / batch_size)
+    for i in range(n_batches):
+        passages = [p for p in passages_set[i * batch_size: (i + 1) * batch_size]["passage_text"]]
+        reps = embed_passages_for_retrieval(passages, qa_tokenizer, qa_embedding, max_length, device)
+        fp[i * batch_size: (i + 1) * batch_size] = reps
+        if i % 50 == 0:
+            print(i, time() - st_time)
+
+
+if __name__ == '__main__':
+    wiki40b_snippets = datasets.load_dataset('wiki_snippets', name='wiki40b_en_100_0')['train']
+    tokenizer, model = make_qa_retriever_model(from_file='models/eli5c_retriever_model_val-1_l-8_h-768_b-512-512_9.pth')
+    make_qa_dense_index(
+        model, tokenizer, wiki40b_snippets, device='cuda:0',
+        index_name='wiki40b_passages_reps_32_l-8_h-768_b-512-512.dat'
+    )
